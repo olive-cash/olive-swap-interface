4ad044145152e7117bd064c947462449b3385951
olive-rose <oliver.rose.bsc@users.noreply.github.com>
2021-04-02 15:08:04 +0400

    Initial 🫒

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,57 @@
+import { MenuEntry } from '@olive-dev/uikit'
+
+const config: MenuEntry[] = [
+  {
+    label: 'Home',
+    icon: 'HomeIcon',
+    href: 'https://olive.cash/farms',
+  },
+  {
+    label: 'Trade',
+    icon: 'TradeIcon',
+    initialOpenState: true,
+    items: [
+      {
+        label: 'Exchange',
+        href: '/swap',
+      },
+      {
+        label: 'Liquidity',
+        href: '/pool',
+      },
+    ],
+  },
+  {
+    label: 'Farms',
+    icon: 'FarmIcon',
+    href: 'https://olive.cash/farms',
+  },
+  {
+    label: 'Pools',
+    icon: 'CaveIcon',
+    href: 'https://olive.cash/pools',
+  },
+  {
+    label: 'More',
+    icon: 'MoreIcon',
+    items: [
+      {
+        label: 'Github',
+        href: 'https://github.com/olive-cash',
+        external: true,
+      },
+      {
+        label: 'Docs',
+        href: 'https://olive-cash.medium.com',
+        external: true,
+      },
+      {
+        label: 'Blog',
+        href: 'https://olive-cash.medium.com',
+        external: true,
+      },
+    ],
+  },
+]
+
+export default config
